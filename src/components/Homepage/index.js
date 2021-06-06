befc37bd52e3569cfaccd4288d1f2991106ea649
tsahiBarshevsky <tsahi.13@gmail.com>
2021-06-06 16:06:25 +0300

    Peimary ticker

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import NewsTicker from 'react-advanced-news-ticker';
+import { posts } from '../demmyPosts';
 
 export default function Homepage() 
 {
     return (
-        <div className="page-container">
-            <div className="hero-container">
-                
-            </div>
-            <div style={{height: 300}}></div>
+        <div className="homepage-container">
+            <div className="hero-container"></div>
+            <NewsTicker 
+                maxRows={1} 
+                rowHeight={35}
+                speed={1000}
+                className="ticker">
+                {posts.map((post) =>
+                    <div key={post.id}>
+                        <h2>{post.date} | {post.title}</h2>
+                    </div>
+                )}
+            </NewsTicker>
         </div>
     )
 }
