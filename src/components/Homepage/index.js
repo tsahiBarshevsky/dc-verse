1a50f60c4f9805ab19aa21831a888ba4669fccf0
tsahiBarshevsky <tsahi.13@gmail.com>
2021-06-06 11:36:52 +0300

    Hero container

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default function Homepage() {
+export default function Homepage() 
+{
     return (
         <div className="page-container">
-            hey
+            <div className="hero-container">
+                
+            </div>
+            <div style={{height: 300}}></div>
         </div>
     )
 }
